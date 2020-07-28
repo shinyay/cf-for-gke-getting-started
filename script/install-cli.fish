74f9be8763e30a9f2a6338ced767f72d1dd53f36
shinyay <shinya.com@gmail.com>
2020-07-28 20:58:19 +0900

    Add retrieve cli for cf for k8s

---
+++ script/install-cli.fish
@@ -0,0 +1,13 @@
+#!/usr/bin/env fish
+
+curl -L "https://packages.cloudfoundry.org/stable?release=linux64-binary&source=github" | tar -zx
+rm LICENSE NOTICE 
+
+curl -o bosh https://github.com/cloudfoundry/bosh-cli/releases/download/v6.3.1/bosh-cli-6.3.1-linux-amd64
+chmod +x bosh
+
+curl -o ytt https://github.com/k14s/ytt/releases/download/v0.28.0/ytt-linux-amd64
+chmod +x ytt
+
+curl -o kapp https://github.com/k14s/kapp/releases/download/v0.31.0/kapp-linux-amd64
+chmod +x kapp
