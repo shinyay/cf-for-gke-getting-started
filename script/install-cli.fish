e5a2cd1770ab52bd845f0c3dd54eeca76153859b
shinyay <shinya.com@gmail.com>
2020-07-29 09:05:21 +0900

    Modify curl option to download

--- script/install-cli.fish
+++ script/install-cli.fish
@@ -3,13 +3,13 @@
 curl -L "https://packages.cloudfoundry.org/stable?release=linux64-binary&source=github" | tar -zx
 rm LICENSE NOTICE 
 
-curl -o bosh https://github.com/cloudfoundry/bosh-cli/releases/download/v6.3.1/bosh-cli-6.3.1-linux-amd64
+curl -o bosh -L https://github.com/cloudfoundry/bosh-cli/releases/download/v6.3.1/bosh-cli-6.3.1-linux-amd64
 chmod +x bosh
 
-curl -o ytt https://github.com/k14s/ytt/releases/download/v0.28.0/ytt-linux-amd64
+curl -o ytt -L https://github.com/k14s/ytt/releases/download/v0.28.0/ytt-linux-amd64
 chmod +x ytt
 
-curl -o kapp https://github.com/k14s/kapp/releases/download/v0.31.0/kapp-linux-amd64
+curl -o kapp -L https://github.com/k14s/kapp/releases/download/v0.31.0/kapp-linux-amd64
 chmod +x kapp
 
 set PATH (pwd |awk -F '/cf-for-gke-getting-started' '{print $1}')/cf-for-gke-getting-started/script:$PATH
